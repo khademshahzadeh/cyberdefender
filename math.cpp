0c475d7e7a259afc370332bcca0ec1d487cfa982
Sabet <sep.mirshahi@gmail.com>
2021-05-22 10:54:17 +0430

    result calculated in calculate function

--- math.cpp
+++ math.cpp
@@ -10,20 +10,8 @@ class math{
         vector<int> number;
         vector<int> operatornum;
         vector<char> operators;
-        int result;
+        int result=0;
     public:
-        int priorityCheck(){
-            for (int i=0;i<operatornum.size();i++){
-                if(operatornum[i]==2){
-                    number[i]=number[i]*number[i+1];
-                    operatornum.erase(operatornum.begin()+i);
-                    operators.erase(operators.begin()+i);
-                    number.erase(number.begin()+i+1);
-                    i--;
-                }
-            }
-        }
-        
         math(){
             srand(time(0));
             for (int i=0; i<9;i++){
@@ -54,6 +42,33 @@ class math{
                 operators[operatornum.size()-1]='-';
             }
         };
+
+        int priorityCheck(){
+            for (int i=0;i<operatornum.size();i++){
+                if(operatornum[i]==2){
+                    number[i]=number[i]*number[i+1];
+                    operatornum.erase(operatornum.begin()+i);
+                    operators.erase(operators.begin()+i);
+                    number.erase(number.begin()+i+1);
+                    i--;
+                }
+            }
+        }
+
+        int calculate(){
+            priorityCheck();
+            result+=number[0];
+            for (int i=0; i<operatornum.size(); i++){
+                if (operatornum[i]==0){
+                    result+=number[i+1];        
+                }
+                else{
+                    result-=number[i+1];        
+                }
+            }
+            cout<<result;
+        }
+
         print(){
             for (int i=0;i<operators.size();i++){
                 cout<<number[i]<<operators[i];

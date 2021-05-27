e0ba7447e6992aa423087a53b4b9b43fc3bff00c
Sabet <sep.mirshahi@gmail.com>
2021-05-27 13:26:36 +0430

    checkAnswer function added, The program looks complete!

--- Scramble.cpp
+++ Scramble.cpp
@@ -32,21 +32,33 @@ private:
         scrambleWord=unscrambleWord;
         random_shuffle(scrambleWord.begin(),scrambleWord.end());
     }
-
 public:
     Scramble(){
         setEncodedWord();
         encodedToUnscramble();
         unscrambleToScramble();
     };
+    bool checkAnswer(){
+        string answer;
+        cin>>answer;
+        if (answer==unscrambleWord){
+            cout<<"Your answer was correct. Let's go for the next round!"<<endl;
+            return true;
+        }
+        else{
+            cout<<"Wrong!"<<endl<<"You can do better dude!"<<endl<<"Try again but you lost one of your chances!"<<endl;
+            return false;
+        }
+    }
 };
 ostream &operator <<(ostream &out,Scramble in){
-    out<<in.scrambleWord;
+    out<<in.scrambleWord<<endl;
     return out;
 }
 
 int main(){
     Scramble test;
     cout<<test;
+    test.checkAnswer();
     return 0;
 }

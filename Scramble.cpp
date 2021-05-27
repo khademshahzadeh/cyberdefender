456d82c51896ee2382521a739aeee55136d1db7f
Sabet <sep.mirshahi@gmail.com>
2021-05-27 12:54:21 +0430

    .gitignore added, operator << overloaded, setEncodedWord function added

--- Scramble.cpp
+++ Scramble.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 class Scramble
 {
+    friend ostream &operator <<(ostream &,Scramble );
 private:
-    
+    string encodedWord;
+    string unscrambleWord;
+    string scrambleWord;
+    void setEncodedWord(){
+        srand(time(0));
+        int randWord=rand()%499;
+        ifstream wordlist("words.dat",ios::in);
+        for (int i = 0; i < randWord; i++)
+        {
+            wordlist>>encodedWord;
+        }
+    };
 public:
-    Scramble();
-    ~Scramble();
+    Scramble(){
+      setEncodedWord();
+
+    };
 };
+ostream &operator <<(ostream &out,Scramble in){
+    out<<in.encodedWord;
+    return out;
+}
 
 int main(){
-    system("PAUSE");
+    Scramble test;
+    cout<<test;
     return 0;
 }

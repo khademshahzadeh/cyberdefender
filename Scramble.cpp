880b7fd889bdc867c2d804db55ec5f468c63a0df
Sabet <sep.mirshahi@gmail.com>
2021-05-27 11:44:43 +0430

    a list of 500 words gathered and coded by int(index)+i in words.dat, cpp file added with the class Scramble

---
+++ Scramble.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Scramble
+{
+private:
+    
+public:
+    Scramble();
+    ~Scramble();
+};
+
+int main(){
+    system("PAUSE");
+    return 0;
+}
